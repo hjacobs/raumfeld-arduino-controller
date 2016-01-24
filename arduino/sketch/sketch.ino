924e8f0baaca9a60bf999f4192c39b8864049d15
Henning Jacobs <henning@jacobs1.de>
2016-01-24 18:03:38 +0100

    allow jumping to previous and next track

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -6,9 +6,20 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int maxVolume = 60;
 
+int reading;
+int button1;
+int lastButton1;
+long lastDebounce1;
+int button2;
+int lastButton2;
+int lastDebounce2;
+long debounceDelay = 50;
 int prevVal = -1;
 
 void setup() {
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+
     Serial.begin(9600);
     // set up the number of columns and rows on the LCD
     lcd.begin(16, 2);
@@ -19,20 +30,59 @@ void setup() {
 
 void loop() {
     // check the status of the switch
+    reading = digitalRead(8);
+
+    if (reading != lastButton1) {
+        lastDebounce1 = millis();
+    }
+
+    if ((millis() - lastDebounce1) > debounceDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+
+        // if the button state has changed:
+        if (reading != button1) {
+            button1 = reading;
+
+            if (button1 == LOW) {
+                Serial.println("prev");
+            }
+        }
+    }
+    lastButton1 = reading;
+
+    // next
+    reading = digitalRead(9);
+
+    if (reading != lastButton2) {
+        lastDebounce2 = millis();
+    }
+
+    if ((millis() - lastDebounce2) > debounceDelay) {
+        // if the button state has changed:
+        if (reading != button2) {
+            button2 = reading;
+
+            if (button2 == LOW) {
+                Serial.println("next");
+            }
+        }
+    }
+    lastButton2 = reading;
+
     unsigned long val = (1023 - analogRead(A0));
-    // val = map(val, 0, 1023, 0, 100);
     val = sqrt(val * 10) * 10.13; // 0 - 1024
     val = val * maxVolume/1024;
- 
-  
+
     if (val != prevVal) {
         if (prevVal == -1)
         {
             lcd.clear();
             lcd.print("Volume: ");
         }
+        Serial.print("volume=");
         Serial.println(val);
-      
+
         // set the cursor to column 0, line 0
         lcd.setCursor(9, 0);
         lcd.print(val);
@@ -47,19 +97,19 @@ void loop() {
         }
         prevVal = val;
         delay(50);
- 
-    } else if (Serial.available()) {
-    // wait a bit for the entire message to arrive
-    delay(100);
-    // clear the screen
-    lcd.clear();
-    // read all the available characters
-    while (Serial.available() > 0) {
-      // display each character to the LCD
-      lcd.write(Serial.read());
-    }
-  }
-    delay(20);
- 
-}
 
+
+    } else if (Serial.available()) {
+        // wait a bit for the entire message to arrive
+        delay(100);
+        // clear the screen
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        // read all the available characters
+        while (Serial.available() > 0) {
+            // display each character to the LCD
+            lcd.write(Serial.read());
+        }
+    }
+    delay(20);
+}
--- raspberry/main.py
+++ raspberry/main.py
@@ -2,25 +2,39 @@
 
 import raumfeld
 import serial
+import sys
 import time
 from pysimplesoap.simplexml import SimpleXMLElement
 
 devices = raumfeld.discover()
 dev = devices[0]
 
-ser = serial.Serial('/dev/ttyACM0')
+ser = serial.Serial(sys.argv[1])
 while True:
-    pos = dev.av_transport.GetPositionInfo(InstanceID=0)
-    metadata = SimpleXMLElement(str(pos.TrackMetaData))
-    track = {}
-    for elem in metadata.item.children():
-        track[elem.get_name()] = str(elem)
-
-    text = '{} - {}'.format(track.get('upnp:artist'), track.get('dc:title'))
-    ser.write(text.encode('utf-8'))
-    val = int(ser.readline().strip())
-    print('Setting volume to {}..'.format(val))
-    dev.volume = val
+    pos = dev.av_transport.GetPositionInfo()
+    try:
+        metadata = SimpleXMLElement(str(pos.TrackMetaData))
+    except:
+        metadata = None
+    if metadata:
+        track = {}
+        for elem in metadata.item.children():
+            track[elem.get_name()] = str(elem)
+        text = '{} - {}'.format(track.get('upnp:artist'), track.get('dc:title'))
+        ser.write(text.encode('utf-8'))
+    val = ser.readline().strip()
+    print(val)
+    key, sep, val = val.partition(b'=')
+    if key == b'volume':
+        val = int(val)
+        print('Setting volume to {}..'.format(val))
+        dev.volume = val
+    elif key == b'prev':
+        print('Previous track..')
+        dev.av_transport.Previous()
+    elif key == b'next':
+        print('Next track..')
+        dev.av_transport.Next()
     time.sleep(0.1)
 
 
